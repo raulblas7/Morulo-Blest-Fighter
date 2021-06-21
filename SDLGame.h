dc798233efc0ef2fd5b05dd0d90d0fa91da423c9
raulblas7 <48948743+raulblas7@users.noreply.github.com>
2021-06-21 20:33:50 +0200

    Clases que git decidio borrar añadidas
    Co-Authored-By: AnassCarreau <47498100+AnassCarreau@users.noreply.github.com>
    Co-Authored-By: Amaro Blest Polo <36236348+Sounagix@users.noreply.github.com>

---
+++ SDLGame.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include "TexturesManager.h"
+#include "Constants.h"
+
+class SDLGame
+{
+
+public:
+  static SDLGame *GetInstance();
+
+  SDLGame(SDLGame &other) = delete;
+  void operator=(const SDLGame &) = delete;
+
+
+  TexturesManager *getTextureManager() const { return textureManager_; }
+  SDL_Window *getWindow() const { return window_; };
+  SDL_Renderer *getRenderer() const { return renderer_; };
+  ~SDLGame();
+  SDLGame();
+
+private:
+  void initGame();
+  void initResources();
+  void initInputHandler();
+  SDL_Window *window_ = nullptr;
+  SDL_Renderer *renderer_ = nullptr;
+  TexturesManager *textureManager_ = nullptr;
+
+  static SDLGame *instance;
+};
